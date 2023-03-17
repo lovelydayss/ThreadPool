dc8f6825fba5e0ce5737ca49eab94be58231d60f
lovelydayss <137208403@qq.com>
2023-03-18 01:35:19 +0800

    3.18

--- src/Utils/ThreadPool/UThreadPool.cpp
+++ src/Utils/ThreadPool/UThreadPool.cpp
@@ -3,7 +3,24 @@
 
 THREADPOOL_NAMESPACE_BEGIN
 
+// 通过默认设置参数创建线程池
+UThreadPool::UThreadPool (Bool autoInit,const UThreadPoolConfig &config) noexcept{
+    cur_index_ = 0;
+    is_init_ = false;
+    input_task_num_ = 0;
 
+    // setConfig 函数，用在 init 设定之后
+    this->setConfig(config);
+
+    // 根据参数设定，决定是否开启监控线程。默认开启 
+    is_monitor_ = config_.monitor_enable_; 
+    monitor_thread_ = std::move(std::thread(&UThreadPool::monitor, this));
+    
+    if (autoInit) {
+        this->init();
+    }
+
+}
 
 
 
